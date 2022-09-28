fbfe7cdb40d5340ffba95089a3ff186b29cff3c6
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-09-28 19:38:36 -0300

    Fix player movement

--- script.js
+++ script.js
@@ -6,54 +6,60 @@ const CANVAS_HEIGHT = (canvas.height = 500);
 const keys = [];
 
 const player = {
-	x: 200,
-	y: 200,
-	width: 40,
-	height: 72,
-	frameX: 0,
-	frameY: 0,
-	speed: 9,
-	moving: false
-}
+  x: 200,
+  y: 200,
+  width: 40,
+  height: 72,
+  frameX: 0,
+  frameY: 0,
+  speed: 9,
+  moving: false,
+};
 
 const playerSprite = new Image();
-playerSprite.src = "chewie.png"
+playerSprite.src = "chewie.png";
 const background = new Image();
-background.src = "tattooine_game_background.png" 
+background.src = "tattooine_game_background.png";
 
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
-	ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
 function animate() {
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-	drawSprite(
-		playerSprite,
-		player.width * player.frameX,
-		player.height * player.frameY,
-		player.width,
-		player.height,
-		player.x,
-		player.y,
-		player.width,
-		player.height
-	);
-	requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  drawSprite(
+    playerSprite,
+    player.width * player.frameX,
+    player.height * player.frameY,
+    player.width,
+    player.height,
+    player.x,
+    player.y,
+    player.width,
+    player.height
+  );
+  requestAnimationFrame(animate);
 }
 animate();
 
-window.addEventListener("keydown", function(e){
-	keys[e.keyCode] = true;
+window.addEventListener("keydown", function (e) {
+  keys[e.key] = true;
+  movePLayer();
+  console.log(e.key);
 });
 
-window.addEventListener("keyup", function(e){
-	delete keys[e.keyCode];
+window.addEventListener("keyup", function (e) {
+  delete keys[e.key];
 });
 
 function movePLayer() {
- // move up without crossing screen limits
-	if(keys[38] && player.y > 100) {
-		player.y -=player.speed;
-		player.frameY = 3
-	}
+  // move up without crossing screen limits
+  if (keys["w"] && player.y > 100) {
+    player.y -= player.speed;
+    player.frameY = 3;
+  } else if (keys["s"] && player.y) {
+    player.y += player.speed;
+    player.frameY = 3;
+  }
 }
